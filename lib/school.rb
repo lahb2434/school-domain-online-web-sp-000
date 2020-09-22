2a18be297013378cffd2cfba5f5fc870bee0afc1
Chris Houghton <chrishoughton2434@gmail.com>
2020-09-22 13:50:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+def dumb(name, grade)
+   if (1..12).include? grade
+      if @a.has_key?(grade)
+         @a[grade] << name
+      else
+         @a[grade] = []
+         @a[grade] << name
+      end
+   else
+      puts "Grade out of scope"
+   end
+end# code here!
